f785aa4c84955f35621dda4db6d84465f8186b2a
jacobburk <jacobburk13@gmail.com>
2025-05-02 08:51:21 -0400

    changed profile to stats

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
       <div className="container mx-auto flex justify-center">
         <div className="flex space-x-6 text-lg">
           <a href="/home" className="text-red-600 hover:text-red-400 transition-all">Home</a>
-          <a href="/privateUserProfile" className="text-red-600 hover:text-red-400 transition-all">Profile</a>
+          <a href="/privateUserProfile" className="text-red-600 hover:text-red-400 transition-all">Stats</a>
           <a href="/practice" className="text-red-600 hover:text-red-400 transition-all">Practice</a>
           <a href="/basic-strategy" className="text-red-600 hover:text-red-400 transition-all">Basic Strategy Guide</a>
           <a href="/card-counting" className="text-red-600 hover:text-red-400 transition-all">Card Counting Guide</a>
